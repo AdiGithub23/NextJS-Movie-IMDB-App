f7eb9dc4e64415a27df2e7f46aa8647d4bf8cffd
AdiGithub23 <gunathilakeadheesha23@gmail.com>
2024-07-01 13:23:35 +0530

    About Page Done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-5'>
+      <h1 className='text-2xl font-medium text-amber-600'>About Us</h1>
+      <p>
+        Welcome to our movie database! 
+        We're a team of movie enthusiasts dedicated to providing a comprehensive, 
+        constantly updated collection of films from around the globe. 
+        Search by title, director, actor, genre, or release date to find your perfect movie.
+      </p>
+      <p>
+        Join our community to discuss films, read reviews, 
+        and stay updated with the latest news and trailers. 
+        Thank you for visiting, and enjoy exploring our site!
+      </p>
+    </div>
+  );
+}
