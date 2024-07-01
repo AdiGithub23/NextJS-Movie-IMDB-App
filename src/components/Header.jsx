d1215cb740dd0c02035026dc062e794dd918932b
AdiGithub23 <gunathilakeadheesha23@gmail.com>
2024-07-01 13:10:10 +0530

    dark mode - done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
-import MenuItem from "./MenuItem"
+import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
-
-export default function Header(){
+import DarkModeSwitch from "@/components/DarkModeSwitch";
+export default function Header() {
   return (
-    <div className="flex justify-between items-center p-3
-      max-w-6xl mx-auto">
+    <div
+      className="flex justify-between items-center p-3
+      max-w-6xl mx-auto"
+    >
       <div className="flex gap-5">
-        <MenuItem title='home' address='/' Icon={AiFillHome}/>
-        <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill}/>
+        <MenuItem title="home" address="/" Icon={AiFillHome} />
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={'/'} className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDB</span>
-        <span className="text-xl hidden sm:inline">Movie App</span>
-      </Link>
+
+      <div className="flex gap-5 items-center">
+        <DarkModeSwitch />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            IMDB
+          </span>
+          <span className="text-xl hidden sm:inline">Movie App</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
